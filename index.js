2015994ffeaa4552f33e27aaf6175bec912f9d45
mjbitoon <105916626+mjbitoon@users.noreply.github.com>
2022-10-06 14:10:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+//when bUttons got click :
+var btn = document.querySelectorAll(".drum");
+
+for(var i = 0 ; i < btn.length; i++){
+
+  btn[i].addEventListener("click", function () {
+
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+//when keys(letters in the buttons) are pressed :
+document.addEventListener("keypress", function(event){
+
+makeSound(event.key);
+});
+
+
+//Function to create sound :
+
+function makeSound (key) {
+
+switch (key) {
+  case "w":
+  var crash = new Audio('sounds/crash.mp3');
+  crash.play();
+    break;
+    case "a":
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+      break;
+      case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+        break;
+        case "d":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+          break;
+          case "j":
+          var tom2 = new Audio('sounds/tom-2.mp3');
+          tom2.play();
+          break;
+          case "k":
+          var tom3= new Audio('sounds/tom-3.mp3');
+          tom3.play();
+            break;
+            case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+              break;
+
+  default: console.log(buttonInnerHTML);
+
+}
+}
